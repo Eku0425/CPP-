e955099851603b19e1ef25bd94c349f7e3a790c8
Ekta Trivedi <149374328+Eku0425@users.noreply.github.com>
2024-01-30 14:55:47 +0530

    Add files via upload

---
+++ exam c++/2.cpp
@@ -0,0 +1,36 @@
+//Write a Program to create a class to read time in seconds and convert it into time in (HH:MM: SS) format.
+//For example,
+
+#include<iostream>
+
+using namespace std;
+
+
+class Time
+{ 
+    int sec,hour,min;
+    
+    
+		
+};
+
+main()
+{
+	
+	 int totalsec;
+	 
+	 
+	cout<<"enter the totalsec:";
+	cin>> totalsec;
+	
+	
+	 
+  int	 hour=totalsec/3600;
+  totalsec=totalsec%3600;
+  int	min=totalsec/60;
+  int	sec=totalsec/3600;
+	
+	cout<<"hour"<<endl<<hour<<"min"<<endl<<min<<endl<<"sec"<<endl<<sec;
+	
+}
+
---
+++ exam c++/Un1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Add
+{
+	int feet ,inch;
+};
+
+main()
+{
+	int feet1,inch1;
+	int feet2, inch2;
+	
+	cout<<"enter the feet:";
+	cin>>feet1;
+	
+	cout<<"enter the inch:";
+	cin>>inch1;
+	
+	feet=inch/12;
+	feet=feet+1;
+	
+	inch=inch(feet*12);
+	
+}
