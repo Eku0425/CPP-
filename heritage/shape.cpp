12b6faf2b00b0e8f8f9011f672bbf1d5f68a0d22
Ekta Trivedi <149374328+Eku0425@users.noreply.github.com>
2024-02-12 23:13:49 +0530

    Add files via upload

---
+++ heritage/shape.cpp
@@ -0,0 +1,66 @@
+// Write a Program that defines a shape class with a constructor that gives value to width and height.
+// Then define two sub-classes triangle and rectangle, that calculate the area of the shape area ().
+ // In the main, define two objects a triangle and a rectangle, and then call the area() 
+  // function using these two objects.
+  
+#include<iostream>
+using namespace std;
+
+class Shape
+{
+	  public:
+	  	
+	  	double height, width;
+	  	
+	  	void get ()
+	  	{
+	  		cout<<"enter the height:";
+	  		cin>>height;
+	  		
+	  		cout<<"enter the width:";
+	  		cin>>width;
+	  		
+	    }
+};
+
+class Rectangle : public  Shape
+{
+	  public:
+	  	
+	  	void area()
+	  	{
+	  		 cout<<"RECTANGLE:"<<endl;
+	  		 get();
+	  		 
+	  		 cout<<"rectangle is: "<< height *width<< endl<<endl;
+		 }
+};
+
+class Triangle : public  Shape
+{
+	  public:
+	  	
+	  	void area()
+	  	{
+	  		 cout<<"TRIANGLE:"<<endl;
+	  		 get();
+	  		 
+	  		 cout<<"trinagle is: "<< (height * width)/2<< endl;
+		 }
+};
+
+int main()
+{
+	Rectangle r1;
+	Triangle t1;
+	r1.area();
+	t1.area();
+
+	return 0;
+}
+
+
+
+	
+	  
+
